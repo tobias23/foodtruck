d98865de130a91bc942245cda1b849513546949b
Hede92 <Hede92@gmail.com>
2017-12-04 12:58:45 +0100

    Test af stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,11 +6,13 @@ import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
+import { AbePage } from '../pages/abe/abe';
 
 @NgModule({
   declarations: [
     MyApp,
-    HomePage
+    HomePage,
+    AbePage
   ],
   imports: [
     BrowserModule,
@@ -19,7 +21,8 @@ import { HomePage } from '../pages/home/home';
   bootstrap: [IonicApp],
   entryComponents: [
     MyApp,
-    HomePage
+    HomePage,
+    AbePage
   ],
   providers: [
     StatusBar,

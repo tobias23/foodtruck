f5f1fe19c7767d570aed747855dffe46d6795d93
Hede92 <Hede92@gmail.com>
2017-12-05 15:31:41 +0100

    Map med markers og infovinduer

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 import {Geolocation} from "@ionic-native/geolocation";
+import { HttpModule } from "@angular/http";
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
@@ -16,7 +17,8 @@ import { HomePage } from '../pages/home/home';
   ],
   imports: [
     BrowserModule,
-    IonicModule.forRoot(MyApp)
+    IonicModule.forRoot(MyApp),
+    HttpModule
   ],
   bootstrap: [IonicApp],
   entryComponents: [
@@ -27,6 +29,7 @@ import { HomePage } from '../pages/home/home';
     StatusBar,
     SplashScreen,
     Geolocation,
+    HttpModule,
     {provide: ErrorHandler, useClass: IonicErrorHandler}
   ]
 })
--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,7 +1,8 @@
 import { Component, ViewChild, ElementRef } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {Geolocation} from "@ionic-native/geolocation";
-
+import { Http } from "@angular/http";
+import 'rxjs/add/operator/map';
 
 declare var google;
 
@@ -11,15 +12,17 @@ declare var google;
 })
 export class HomePage {
 
+
+
   @ViewChild('map') mapElement: ElementRef;
   map: any;
 
-  constructor(public navCtrl: NavController, public geolocation: Geolocation) {
-
+  constructor(public navCtrl: NavController, public geolocation: Geolocation, public http: Http) {
   }
 
-  ionViewDidLoad(){
-    this.loadMap();
+  ionViewWillEnter() {
+    this.loadMap()
+
   }
 
   loadMap(){
@@ -35,28 +38,41 @@ export class HomePage {
 
       };
       this.map = new google.maps.Map(this.mapElement.nativeElement, mapOptions);
-      var geomarker = new Geolocation
+
+      let marker = new google.maps.Marker({
+        map: this.map,
+        animation: google.maps.Animation.DROP,
+        position: latLng
+      });
+      let content = "<h4>Her er jeg.</h4>";
+
+      this.addInfoWindow(marker, content);
+
+      this.getMarkers();
 
     }, (err) => {
       console.log(err);
     });
-
-
-
-
   };
 
 
 
-  addMarker(){
-    let marker = new google.maps.Marker({
-      map: this.map,
-      animation: google.maps.Animation.DROP,
-      position: this.map.getCenter()
-    });
-    let content = "<h4>Information!</h4>";
+  getMarkers() {
+    this.http.get('assets/data/markers.json').map((res) => res.json()).subscribe(data => {console.log(data)});
+    this.http.get('assets/data/markers.json').map((res) => res.json()).subscribe(data => this.addMarkersToMap(data));
+  };
 
-    this.addInfoWindow(marker, content);
+
+  addMarkersToMap(markers) {
+    console.log("abe");
+    for (let marker of markers) {
+      let position = new google.maps.LatLng(marker.latitude, marker.longitude);
+      let foodtruckMarker = new google.maps.Marker({position: position, title: marker.name, icon: {url: 'assets/imgs/Foodtruck.png', scaledSize: { width: 75, height: 75}}});
+      console.log(position.lat());
+      foodtruckMarker.setMap(this.map);
+      let content = marker.stuff;
+      this.addInfoWindow(foodtruckMarker, content);
+    }
   }
 
   addInfoWindow(marker, content){
@@ -70,4 +86,5 @@ export class HomePage {
     });
 
   }
+
 }
